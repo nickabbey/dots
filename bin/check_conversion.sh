4fb9463f8d8d46edfbe2a64afddcad2161d929bf
Nick Abbruzzese <nick.abbey@regionalhelpwanted.com>
2015-10-21 19:15:30 +0000

    added bin/chech_conversions.sh

---
+++ bin/check_conversion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+TODAY=`date +"%Y-%m-%d"`
+mkdir -p /tmp/conversion_errors
+grep $TODAY /var/log/celery/celeryd-conversion.log | grep "conversion problem" > /tmp/conversion_errors/failed.$TODAY
+sed -r 's/.*for//' /tmp/conversion_errors/failed.$TODAY | sed -r 's/:.*//' > /tmp/conversion_errors/all_ids.$TODAY
+sort -un /tmp/conversion_errors/all_ids.$TODAY > ~/failed_resume_conversion_ids.$TODAY
+rm -rf /tmp/conversion_errors
